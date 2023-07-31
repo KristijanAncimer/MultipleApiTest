313581f0de3849ba2d9b312ac541e4103d78a040
Kristijan Ancimer <kristijan.ancimer@rokolabs.com>
2023-07-31 09:14:15 +0200

    Configuration setuo.

--- AdminApi/Program.cs
+++ AdminApi/Program.cs
@@ -3,8 +3,20 @@ using Microsoft.Extensions.Configuration;
 using MongoDB.Driver.Core.Configuration;
 using System.Xml.Linq;
 
+string mongoConnectionString = Environment.GetEnvironmentVariable("MONGO_CONNECTION_STRING");
+string mongoDatabase = Environment.GetEnvironmentVariable("MONGO_DATABASE");
+
+if (string.IsNullOrEmpty(mongoConnectionString))
+{
+    throw new ArgumentException(" Mongo connection string must be set");
+}
+if (string.IsNullOrEmpty(mongoDatabase))
+{
+    throw new ArgumentException(" Mongo database must be set");
+}
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSingleton<IGenericRepository>(new MongoRepository("mongodb://mamongo:27017", "AdminDatabase"));
+builder.Services.AddSingleton<IGenericRepository>(new MongoRepository(mongoConnectionString, mongoDatabase));
 // Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
