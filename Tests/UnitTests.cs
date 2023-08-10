885cc18f921e1b44f070e30ab2e6965016cf1eed
Kristijan Ancimer <kristijan.ancimer@rokolabs.com>
2023-08-10 11:47:20 +0200

    More stuff I forgot

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -1,64 +1,65 @@
+// <copyright file="UnitTests.cs" company="ROKO Labs, LLC">
+//
+// Copyright (C) ROKO Labs, LLC - All Rights Reserved.
+// Unauthorized copying of this file, via any medium is strictly prohibited. Proprietary and confidential.
+//
+// </copyright>
+
+namespace Tests;
 using Common;
 using Common.Models;
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
-using MongoDB.Driver;
-using System.Collections.ObjectModel;
-using System.Reflection.Emit;
 
-namespace Tests
+public class UnitTests
 {
-
-    public class UnitTests
+    private readonly InMemoryRepository _repository;
+    public UnitTests()
     {
-        InMemoryRepository _repository;
-        public UnitTests()
+        _repository = new InMemoryRepository();
+    }
+
+    [Fact]
+    public void SaveOrUpdates_Succeeds()
+    {
+        // kreirati 1 tenanta i usera
+    }
+
+    [Fact]
+    public async void GetAllSucceeds()
+    {
+        var entities1 = new List<EntityTest1>
         {
-            _repository=new InMemoryRepository();
-        }
-        [Fact]
-        public void SaveOrUpdates_Succedess()
+            new EntityTest1 { Id = Guid.NewGuid(), Name = "name1", TestString = "testblablabla1" },
+            new EntityTest1 { Id = Guid.NewGuid(), Name = "name2", TestString = "testblablabla2" },
+            new EntityTest1 { Id = Guid.NewGuid(), Name = "name3", TestString = "testblablabla3" },
+            new EntityTest1 { Id = Guid.NewGuid(), Name = "name4", TestString = "testblablabla4" },
+            new EntityTest1 { Id = Guid.NewGuid(), Name = "name5", TestString = "testblablabla5" }
+        };
+        var entities2 = new List<EntityTest2>
         {
-            // kreirati 1 tenanta i usera
+            new EntityTest2 { Id = Guid.NewGuid(), Name = "name1", TestInteger = 1 },
+            new EntityTest2 { Id = Guid.NewGuid(), Name = "name2", TestInteger = 2 },
+            new EntityTest2 { Id = Guid.NewGuid(), Name = "name3", TestInteger = 3 },
+            new EntityTest2 { Id = Guid.NewGuid(), Name = "name4", TestInteger = 4 },
+            new EntityTest2 { Id = Guid.NewGuid(), Name = "name5", TestInteger = 5 }
+        };
+        foreach (var entity in entities1)
+        {
+            await _repository.SaveOrUpdateAsync(entity);
         }
 
-        [Fact]
-        public async void GetAllSucceds()
+        foreach (var entity in entities2)
         {
-            var entities1 = new List<EntityTest1>
-            {
-                new EntityTest1{Id=Guid.NewGuid(), Name="name1", testString="testblablabla1"},
-                new EntityTest1{Id=Guid.NewGuid(), Name="name2", testString="testblablabla2"},
-                new EntityTest1{Id=Guid.NewGuid(), Name="name3", testString="testblablabla3"},
-                new EntityTest1{Id=Guid.NewGuid(), Name="name4", testString="testblablabla4"},
-                new EntityTest1{Id=Guid.NewGuid(), Name="name5", testString="testblablabla5"}
-            };
-            var entities2 = new List<EntityTest2>
-            {
-                new EntityTest2{Id=Guid.NewGuid(), Name="name1", testInteger=1},
-                new EntityTest2{Id=Guid.NewGuid(), Name="name2", testInteger=2},
-                new EntityTest2{Id=Guid.NewGuid(), Name="name3", testInteger=3},
-                new EntityTest2{Id=Guid.NewGuid(), Name="name4", testInteger=4},
-                new EntityTest2{Id=Guid.NewGuid(), Name="name5", testInteger=5}
-            };
-            foreach (var entity in entities1)
-            {
-                await _repository.SaveOrUpdateAsync(entity);
-            }
-            foreach (var entity in entities2)
-            {
-                await _repository.SaveOrUpdateAsync(entity);
-            }
+            await _repository.SaveOrUpdateAsync(entity);
         }
+    }
 
-        public class EntityTest1 : AbstractEntity
-        {
-            public string? testString { get; set; }
-        }
+    public class EntityTest1 : AbstractEntity
+    {
+        public string? TestString { get; set; }
+    }
 
-        public class EntityTest2:AbstractEntity 
-        {
-            public int testInteger { get; set; }
-        }
+    public class EntityTest2 : AbstractEntity
+    {
+        public int TestInteger { get; set; }
     }
 }
